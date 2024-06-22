83ef9a0daa6162b27d51cd859a39bcdb3fc0621e
Ted Senft <code@robotgryphon.com>
2024-06-22 01:23:51 -0400

    Clean up API buildscripts and align manifest/versions

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -1,8 +1,7 @@
-
 import java.text.SimpleDateFormat
 import java.util.*
 
-val versionMain: String = System.getenv("VERSION") ?: "0.0.0"
+val versionMain: String = System.getenv("VERSION") ?: "9.9.9"
 
 plugins {
     id("java-library")
@@ -40,24 +39,32 @@ tasks.withType<JavaCompile> {
 }
 
 tasks.withType<Jar> {
+    val gitVersion = providers.exec {
+        commandLine("git", "rev-parse", "HEAD")
+    }.standardOutput.asText.get()
+
     manifest {
         val now = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(Date())
-        attributes(mapOf(
+        attributes(
+            mapOf(
                 "Specification-Title" to "Compact Machines - Core API",
                 "Specification-Version" to "1", // We are version 1 of ourselves
                 "Implementation-Title" to "Compact Machines - Core API",
                 "Implementation-Timestamp" to now,
-                "FMLModType" to "GAMELIBRARY"
-        ))
+                "FMLModType" to "GAMELIBRARY",
+                "Minecraft-Version" to mojang.versions.minecraft.get(),
+                "NeoForge-Version" to neoforged.versions.neoforge.get(),
+                "Main-Commit" to gitVersion
+            )
+        )
     }
 }
 
-val PACKAGES_URL = System.getenv("GH_PKG_URL") ?: "https://maven.pkg.github.com/compactmods/compactmachines-core"
+val PACKAGES_URL = System.getenv("GH_PKG_URL") ?: "https://maven.pkg.github.com/compactmods/compactmachines"
 publishing {
     publications.register<MavenPublication>("api") {
         artifactId = "core-api"
         from(components.getByName("java"))
-//        artifact(tasks.named("remapSrgJar"))
     }
 
     repositories {
