7e3a8bb6e8eae26e8d658db75989fcda118f8188
Ted Senft <code@robotgryphon.com>
2024-10-28 09:45:11 -0400

    [CI] Swap announcer to read directly out of manifest instead

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,26 +5,6 @@ plugins {
     alias(neoforged.plugins.moddev).apply(false)
 }
 
-tasks.register("ngInitCM") {
-    doLast {
-        println("ForgeGradle+CM initialized")
-    }
-}
-
-tasks.register("mcVersion") {
-    doLast {
-        val mc = mojang.versions.minecraft.get()
-        println("version=$mc")
-    }
-}
-
-tasks.register("neoVersion") {
-    doLast {
-        val neo = neoforged.versions.neoforge.get()
-        println("version=$neo")
-    }
-}
-
 subprojects {
     repositories {
         mavenLocal()
