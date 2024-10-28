b253454dac339b469ce179ae5d60b332e49e7b3b
Ted Senft <code@robotgryphon.com>
2024-10-27 21:50:50 -0400

    Update to FG6, Gradle 8.8, clean up some buildscript jank

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,17 @@ pluginManagement {
         id("eclipse")
         id("maven-publish")
     }
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+
+        maven("https://maven.minecraftforge.net")
+        maven("https://maven.parchmentmc.org")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
